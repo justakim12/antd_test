e1ae0891aa64c0d458e5423cf58af1ce8d7148b8
JustaKim <rlaruddlf711@naver.com>
2020-11-15 16:03:59 +0900

    Add layout

--- src/index.js
+++ src/index.js
@@ -2,17 +2,67 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
-import { DatePicker, Space } from 'antd';
+import { Layout, Menu } from 'antd';
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UserOutlined,
+  UploadOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
 
-const { RangePicker } = DatePicker;
+const { Header, Content, Footer, Sider } = Layout;
 
 ReactDOM.render(
-  <Space direction="vertical" size={12}>
-    <RangePicker />
-    <RangePicker showTime />
-    <RangePicker picker="week" />
-    <RangePicker picker="month" />
-    <RangePicker picker="year" />
-  </Space>,
+  <Layout>
+    <Sider
+      style={{
+        overflow: 'auto',
+        height: '100vh',
+        position: 'fixed',
+        left: 0,
+      }}
+    >
+      <div className="logo" />
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+        <Menu.Item key="1" icon={<UserOutlined />}>
+          nav 1
+        </Menu.Item>
+        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+          nav 2
+        </Menu.Item>
+        <Menu.Item key="3" icon={<UploadOutlined />}>
+          nav 3
+        </Menu.Item>
+        <Menu.Item key="4" icon={<BarChartOutlined />}>
+          nav 4
+        </Menu.Item>
+        <Menu.Item key="5" icon={<CloudOutlined />}>
+          nav 5
+        </Menu.Item>
+        <Menu.Item key="6" icon={<AppstoreOutlined />}>
+          nav 6
+        </Menu.Item>
+        <Menu.Item key="7" icon={<TeamOutlined />}>
+          nav 7
+        </Menu.Item>
+        <Menu.Item key="8" icon={<ShopOutlined />}>
+          nav 8
+        </Menu.Item>
+      </Menu>
+    </Sider>
+    <Layout className="site-layout" style={{ marginLeft: 200 }}>
+      <Header className="site-layout-background" style={{ padding: 0 }} />
+      <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+        <div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
+          ...
+          <br />
+        </div>
+      </Content>
+    </Layout>
+  </Layout>,
   document.getElementById('container'),
 );
