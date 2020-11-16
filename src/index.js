8f5ada33cf314192339d141c874482dd31d0511f
JustaKim <rlaruddlf711@naver.com>
2020-11-16 23:42:27 +0900

    Add table

--- src/index.js
+++ src/index.js
@@ -10,7 +10,80 @@ import {
 } from '@ant-design/icons';
 import { DatePicker, Space } from 'antd';
 import { Pagination } from 'antd';
+import { Table, Tag } from 'antd';
 
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: text => <a>{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: tags => (
+      <>
+        {tags.map(tag => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <Space size="middle">
+        <a>Invite {record.name}</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
 const { Header, Content, Footer, Sider } = Layout;
 const { RangePicker } = DatePicker;
 
@@ -42,13 +115,12 @@ ReactDOM.render(
       <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
         <div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
         <Space direction="vertical" size={12}>
-            <RangePicker />
-          </Space> 
-          <br />
-          <br />
-          <br />
-          <br />
-          <Pagination defaultCurrent={1} total={50} />  
+          <RangePicker />
+        <br/>
+        <br/>
+        <br/>
+        </Space> 
+        <Table columns={columns} dataSource={data} />
         </div>
       </Content>
     </Layout>
