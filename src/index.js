87de5b0414d7ddce0d5b7dbc620b766dd508644b
JustaKim <rlaruddlf711@naver.com>
2020-11-16 23:13:10 +0900

    Add datepicker

--- src/index.js
+++ src/index.js
@@ -4,18 +4,15 @@ import 'antd/dist/antd.css';
 import './index.css';
 import { Layout, Menu } from 'antd';
 import {
-  AppstoreOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  ShopOutlined,
-  TeamOutlined,
   UserOutlined,
   UploadOutlined,
   VideoCameraOutlined,
 } from '@ant-design/icons';
+import { DatePicker, Space } from 'antd';
 import { Pagination } from 'antd';
 
 const { Header, Content, Footer, Sider } = Layout;
+const { RangePicker } = DatePicker;
 
 ReactDOM.render(
   <Layout>
@@ -38,28 +35,18 @@ ReactDOM.render(
         <Menu.Item key="3" icon={<UploadOutlined />}>
           nav 3
         </Menu.Item>
-        <Menu.Item key="4" icon={<BarChartOutlined />}>
-          nav 4
-        </Menu.Item>
-        <Menu.Item key="5" icon={<CloudOutlined />}>
-          nav 5
-        </Menu.Item>
-        <Menu.Item key="6" icon={<AppstoreOutlined />}>
-          nav 6
-        </Menu.Item>
-        <Menu.Item key="7" icon={<TeamOutlined />}>
-          nav 7
-        </Menu.Item>
-        <Menu.Item key="8" icon={<ShopOutlined />}>
-          nav 8
-        </Menu.Item>
       </Menu>
     </Sider>
     <Layout className="site-layout" style={{ marginLeft: 200 }}>
       <Header className="site-layout-background" style={{ padding: 0 }} />
       <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
         <div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
-          ...
+        <Space direction="vertical" size={12}>
+            <RangePicker />
+          </Space> 
+          <br />
+          <br />
+          <br />
           <br />
           <Pagination defaultCurrent={1} total={50} />  
         </div>
